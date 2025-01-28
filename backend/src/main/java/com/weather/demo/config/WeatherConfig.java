658ff9aad0e327dd9d62bb177ce28ffd80cbc112
bee-rather <iamganeshbiradar@gmail.com>
2025-01-28 16:18:25 +0530

    null check

--- backend/src/main/java/com/weather/demo/config/WeatherConfig.java
+++ backend/src/main/java/com/weather/demo/config/WeatherConfig.java
@@ -4,13 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Configuration
 @ConfigurationProperties(prefix = "openweathermap")
 @Getter
 @Setter
 public class WeatherConfig {
+
+    @NotNull
+    @NotEmpty
     private String apiKey;
+
+    @NotNull
+    @NotEmpty
     private String baseUrl;
 
     // Optional: Add validation or custom logic
